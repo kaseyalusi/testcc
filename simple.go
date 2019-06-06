7c8bef3c2a797cdded5909558bf5a26530317543
kasey.alusi <kasey.alusi@workday.com>
2019-06-06 13:14:35 -0700

    fixes

--- simple.go
+++ simple.go
@@ -4,31 +4,32 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 type SimpleChaincode struct {
 }
 
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	initUUID := uuid.New()
 	key := initUUID.String()
 	err := stub.PutState(key, []byte("0"))
 	if err != nil {
-		return nil, err
+		shim.Error(err.Error())
 	}
-	return []byte(key), nil
+	return shim.Success([]byte(key))
 }
 
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	invokeUUID := uuid.New()
 	key := invokeUUID.String()
 	err := stub.PutState(key, []byte("0"))
 	if err != nil {
-		return nil, err
+		shim.Error(err.Error())
 	}
-	return []byte(key), nil
+	return shim.Success([]byte(key))
 }
 
 func main() {
